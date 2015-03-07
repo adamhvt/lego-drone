36ec9fb152df3c3050c4537566171347001d79b4
Adam Horvath <adam.horvath.0827@gmail.com>
2015-03-07 02:23:24 +0100

    uploaded project files

---
+++ LegoBoatCs/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace LegoBoatCs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.addTagetPosButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.longitudeTextBox = new System.Windows.Forms.TextBox();
+            this.latitudeTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.backwardButton = new System.Windows.Forms.Button();
+            this.rightButton = new System.Windows.Forms.Button();
+            this.leftButton = new System.Windows.Forms.Button();
+            this.forwardButton = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.startButton = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.logLabel = new System.Windows.Forms.Label();
+            this.mainTimer = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.addTagetPosButton);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.longitudeTextBox);
+            this.groupBox1.Controls.Add(this.latitudeTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(232, 114);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add target position";
+            // 
+            // addTagetPosButton
+            // 
+            this.addTagetPosButton.Location = new System.Drawing.Point(78, 85);
+            this.addTagetPosButton.Name = "addTagetPosButton";
+            this.addTagetPosButton.Size = new System.Drawing.Size(75, 23);
+            this.addTagetPosButton.TabIndex = 4;
+            this.addTagetPosButton.Text = "Send";
+            this.addTagetPosButton.UseVisualStyleBackColor = true;
+            this.addTagetPosButton.Click += new System.EventHandler(this.addTagetPosButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Longitude";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Latitude";
+            // 
+            // longitudeTextBox
+            // 
+            this.longitudeTextBox.Location = new System.Drawing.Point(78, 47);
+            this.longitudeTextBox.Name = "longitudeTextBox";
+            this.longitudeTextBox.Size = new System.Drawing.Size(148, 20);
+            this.longitudeTextBox.TabIndex = 1;
+            // 
+            // latitudeTextBox
+            // 
+            this.latitudeTextBox.Location = new System.Drawing.Point(78, 20);
+            this.latitudeTextBox.Name = "latitudeTextBox";
+            this.latitudeTextBox.Size = new System.Drawing.Size(148, 20);
+            this.latitudeTextBox.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.backwardButton);
+            this.groupBox2.Controls.Add(this.rightButton);
+            this.groupBox2.Controls.Add(this.leftButton);
+            this.groupBox2.Controls.Add(this.forwardButton);
+            this.groupBox2.Location = new System.Drawing.Point(251, 21);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Arrows";
+            // 
+            // backwardButton
+            // 
+            this.backwardButton.Location = new System.Drawing.Point(65, 71);
+            this.backwardButton.Name = "backwardButton";
+            this.backwardButton.Size = new System.Drawing.Size(75, 23);
+            this.backwardButton.TabIndex = 3;
+            this.backwardButton.Text = "Backward";
+            this.backwardButton.UseVisualStyleBackColor = true;
+            this.backwardButton.Click += new System.EventHandler(this.backwardButton_Click);
+            // 
+            // rightButton
+            // 
+            this.rightButton.Location = new System.Drawing.Point(119, 42);
+            this.rightButton.Name = "rightButton";
+            this.rightButton.Size = new System.Drawing.Size(75, 23);
+            this.rightButton.TabIndex = 2;
+            this.rightButton.Text = "Right";
+            this.rightButton.UseVisualStyleBackColor = true;
+            this.rightButton.Click += new System.EventHandler(this.rightButton_Click);
+            // 
+            // leftButton
+            // 
+            this.leftButton.Location = new System.Drawing.Point(6, 42);
+            this.leftButton.Name = "leftButton";
+            this.leftButton.Size = new System.Drawing.Size(75, 23);
+            this.leftButton.TabIndex = 1;
+            this.leftButton.Text = "Left";
+            this.leftButton.UseVisualStyleBackColor = true;
+            this.leftButton.Click += new System.EventHandler(this.leftButton_Click);
+            // 
+            // forwardButton
+            // 
+            this.forwardButton.Location = new System.Drawing.Point(65, 19);
+            this.forwardButton.Name = "forwardButton";
+            this.forwardButton.Size = new System.Drawing.Size(75, 23);
+            this.forwardButton.TabIndex = 0;
+            this.forwardButton.Text = "Forward";
+            this.forwardButton.UseVisualStyleBackColor = true;
+            this.forwardButton.Click += new System.EventHandler(this.forwardButton_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.resetButton);
+            this.groupBox3.Controls.Add(this.stopButton);
+            this.groupBox3.Controls.Add(this.startButton);
+            this.groupBox3.Location = new System.Drawing.Point(13, 134);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(232, 116);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Control buttons";
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(78, 78);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 2;
+            this.resetButton.Text = "RESET";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(78, 49);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 1;
+            this.stopButton.Text = "STOP";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(78, 20);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.logLabel);
+            this.groupBox4.Location = new System.Drawing.Point(251, 134);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(200, 116);
+            this.groupBox4.TabIndex = 3;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Log";
+            // 
+            // logLabel
+            // 
+            this.logLabel.AutoSize = true;
+            this.logLabel.Location = new System.Drawing.Point(6, 20);
+            this.logLabel.Name = "logLabel";
+            this.logLabel.Size = new System.Drawing.Size(49, 13);
+            this.logLabel.TabIndex = 0;
+            this.logLabel.Text = "log_label";
+            // 
+            // mainTimer
+            // 
+            this.mainTimer.Tick += new System.EventHandler(this.mainTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 265);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "LegoClient";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox longitudeTextBox;
+        private System.Windows.Forms.TextBox latitudeTextBox;
+        private System.Windows.Forms.Button addTagetPosButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button backwardButton;
+        private System.Windows.Forms.Button rightButton;
+        private System.Windows.Forms.Button leftButton;
+        private System.Windows.Forms.Button forwardButton;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label logLabel;
+        private System.Windows.Forms.Timer mainTimer;
+    }
+}
+
---
+++ LegoBoatCs/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Lego;
+using Ice;
+
+namespace LegoBoatCs
+{
+    public partial class Form1 : Form
+    {
+        Communicator communicator;
+        ServerPrx server;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            try
+            {
+                this.communicator = Util.initialize();
+                this.server = ServerPrxHelper.checkedCast(this.communicator.stringToProxy("server:tcp -h lego.amk.uni-obuda.hu -p 8082"));
+            }
+            catch (System.Exception ex)
+            {
+                System.Console.Error.WriteLine(ex);
+            }
+
+            this.mainTimer.Start();
+        }
+
+        private void addTagetPosButton_Click(object sender, EventArgs e)
+        {
+            float latitude = Convert.ToSingle(this.latitudeTextBox.Text);
+            float longitude = Convert.ToSingle(this.longitudeTextBox.Text);
+
+            server.addTargetPosition(latitude, longitude);
+        }
+
+        private void forwardButton_Click(object sender, EventArgs e)
+        {
+            this.server.moveByButton("forward");
+        }
+
+        private void leftButton_Click(object sender, EventArgs e)
+        {
+            this.server.moveByButton("left");
+        }
+
+        private void rightButton_Click(object sender, EventArgs e)
+        {
+            this.server.moveByButton("right");
+        }
+
+        private void backwardButton_Click(object sender, EventArgs e)
+        {
+            this.server.moveByButton("backward");
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            this.server.start();
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            this.server.stop();
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            this.server.reset();
+        }
+
+        private void mainTimer_Tick(object sender, EventArgs e)
+        {
+            this.logLabel.Text = server.getActualData();
+        }
+
+    }
+}
